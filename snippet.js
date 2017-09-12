1ba85dc95aec06cc9d855f1d0dae694996449fc1
Victor Yap <victor.yap@alumni.concordia.ca>
2017-09-12 10:45:15 -0400

    Update snippet.js

--- snippet.js
+++ snippet.js
@@ -1,8 +1,7 @@
 (function() {
-	
-	var favicon = 'qa-favicon'; // <-- change this as needed
+    var favicon = 'qa-favicon'; // <-- change this as needed
 
-	var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
     link.href = 'https://raw.githubusercontent.com/starlocke/favicons/master/'+favicon+'.ico';
